407e0a1a8308417eac5f730db409d93e24994276
Beatrix Klebe <bea@klebe.blog>
2018-12-02 15:49:38 -0500

    Finish Day 1 problem 1, ignore IDEA files

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let sum = BufReader::new(File::open("input.txt").unwrap()).lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .fold(0, |acc, x| acc + x);
+    println!("{}", sum);
 }
