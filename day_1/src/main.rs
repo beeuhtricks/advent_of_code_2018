9c2988160454b98adc5397d98b18cc481c0aead1
Beatrix Klebe <bea@klebe.blog>
2018-12-02 15:51:56 -0500

    Use sum instead of fold

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -2,8 +2,8 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let sum = BufReader::new(File::open("input.txt").unwrap()).lines()
+    let sum: i32 = BufReader::new(File::open("input.txt").unwrap()).lines()
         .map(|line| line.unwrap().parse::<i32>().unwrap())
-        .fold(0, |acc, x| acc + x);
+        .sum();
     println!("{}", sum);
 }
