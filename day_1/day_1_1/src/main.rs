26aef24eb498d507e79b6dfa3813bc119e0ab2cd
Beatrix Klebe <bea@klebe.blog>
2018-12-02 17:51:45 -0500

    Moved input up a directory and finished problem 2

--- day_1/day_1_1/src/main.rs
+++ day_1/day_1_1/src/main.rs
@@ -2,8 +2,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let sum: i32 = BufReader::new(File::open("input.txt").unwrap()).lines()
+    let sum: i32 = BufReader::new(File::open("../input.txt").unwrap()).lines()
         .map(|line| line.unwrap().parse::<i32>().unwrap())
         .sum();
+
     println!("{}", sum);
 }
--- day_1/day_1_2/src/main.rs
+++ day_1/day_1_2/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let diffs: Vec<i32> = BufReader::new(File::open("../input.txt").unwrap()).lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    let mut uniq = HashSet::new();
+    let mut total = 0;
+    uniq.insert(total);
+
+    'outer: loop {
+        for diff in &diffs {
+            total += diff;
+            if uniq.contains(&total) {
+                println!("{}", total);
+                break 'outer;
+            } else {
+                uniq.insert(total);
+            }
+        }
+    }
 }
