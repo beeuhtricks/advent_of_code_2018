1d0ebc6ec686a28a544d414be265e501c1f417d6
Beatrix Klebe <bea@klebe.blog>
2018-12-02 20:00:54 -0500

    Add input and finish day_2_1

--- day_2/day_2_1/src/main.rs
+++ day_2/day_2_1/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let boxes: Vec<String> = BufReader::new(File::open("../input.txt").unwrap())
+        .lines()
+        .map(|s| s.unwrap())
+        .collect();
+
+    let twos = boxes.clone().iter().filter(|s| has_two(hash(s))).count();
+    let threes = boxes.clone().iter().filter(|s| has_three(hash(s))).count();
+
+    println!("{}", twos * threes);
+}
+
+fn hash(box_label: &str) -> HashMap<char, i32> {
+    let mut freqs = HashMap::new();
+
+    for c in box_label.chars() {
+        if freqs.contains_key(&c) {
+            *freqs.get_mut(&c).unwrap() += 1;
+        } else {
+            freqs.insert(c, 1);
+        }
+    }
+
+    freqs
+}
+
+fn has_two(hashed_box: HashMap<char, i32>) -> bool {
+    hashed_box.iter().any(|(_, &v)| v == 2)
+}
+
+fn has_three(hashed_box: HashMap<char, i32>) -> bool {
+    hashed_box.iter().any(|(_, &v)| v == 3)
 }
